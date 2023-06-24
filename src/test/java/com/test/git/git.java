196c1dacf48de44f4fa3b21f21e79be96c8a107f
zhanysch@gmail.com <zhanysch@gmail.com>
2023-06-23 19:54:39 -0500

    after stash

--- src/test/java/com/test/git/git.java
+++ src/test/java/com/test/git/git.java
@@ -4,6 +4,14 @@ public class git {
     public static void main(String[] args) {
         System.out.println("New branch");
 
+<<<<<<< Updated upstream
+
         /* This is change from branch secondBranch */
+
+        System.out.println("sfdsf");
+
+=======
+        System.out.println("sfdsf");
+>>>>>>> Stashed changes
     }
 }
